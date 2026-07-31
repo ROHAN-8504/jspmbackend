ca0fceeae242fa16ece27e6373514c4aa54bd0a7
ROHAN-8504 <203j1a4207@raghuinstech.com>
2026-07-31 16:50:45 +0530

    latest changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express=require('express')
 const app=express();
 const dotenv=require('dotenv')
+let helmet=require('helmet')
 dotenv.config();
 const cors=require('cors')
 let logging=require('./middlewares/logger')
@@ -13,12 +14,20 @@ let authroutes=require('./routes/authroutes')
 app.use(express.json())
 app.use(cors())
 app.use(ratelimit)
-app.use(logging)
+app.use(logging)z
+ap.use(helmet())
 
+app.disable('x-powered-by');
 
 //routes
 app.use('/products',productsroutes)
 app.use('/api',authroutes)
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 const port=process.env.PORT || 5000
 app.listen(port,()=>{
